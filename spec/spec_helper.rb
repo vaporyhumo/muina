f84ebd9662c47ed4f638cc67ad996022522cd026
Rodrigo Vilina <roanvilina@gmail.com>
2025-11-09 17:58:15 -0300

    Add `spec_helper.rb`

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+require 'muina'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.disable_monkey_patching!
+  config.profile_examples = 0
+
+  Kernel.srand config.seed
+
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true
+  end
+end
