00b0d7e7e26570dcb463277164150f909dae7f00
Rodrigo Vilina <roanvilina@gmail.com>
2025-11-09 18:10:25 -0300

    Add `Maybe::none`

--- lib/muina/maybe.rb
+++ lib/muina/maybe.rb
@@ -2,6 +2,19 @@
 
 module Muina
   class Maybe
+    class << self
+      # Returns a {Maybe} of the +None+ variant, a safer alternative to +nil+.
+      #
+      # @return [Maybe]
+      def none
+        new(nil)
+      end
+    end
+
     private_class_method :new
+    def initialize(value)
+      @value = value
+      freeze
+    end
   end
 end
--- spec/muina/maybe_spec.rb
+++ spec/muina/maybe_spec.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 RSpec.describe Muina::Maybe do
+  let(:none) { described_class::none }
+
   describe '.new' do
     specify do
       expect { described_class.new }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe '.none' do
+    specify do
+      expect(none).to be_a(Maybe)
+    end
+
+    specify do
+      expect(none).to be_frozen
+    end
+
+    specify do
+      expect(none.instance_variable_get(:@value)).to be_nil
     end
   end
 end
